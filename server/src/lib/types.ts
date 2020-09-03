361fc99385ad751feaee2d2419d2d97dce5619d6
Ivan Arsenev <ivan_arsenev@pm.me>
2020-09-03 09:05:00 +0300

    Added types for all instance in project, added mock data to seed script

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,15 +1,57 @@
 import { Collection, ObjectId } from "mongodb"
 
+export enum ListingType {
+  Apartment = "APARTMENT",
+  House = "HOUSE",
+}
+
+export interface BookingsIndexMonth {
+  [key: string]: boolean
+}
+
+export interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear
+}
+
 export interface Booking {
   _id: ObjectId
+  listing: ObjectId
+  tenant: string
+  checkIn: string
+  checkOut: string
 }
 
 export interface Listing {
   _id: ObjectId
+  title: string
+  description: string
+  image: string
+  host: string // one to one relationship / to user id
+  type: ListingType
+  address: string
+  country: string
+  admin: string
+  city: string
+  bookings: ObjectId[]
+  bookingsIndex: BookingsIndex
+  price: number
+  numOfGuests: number
 }
 
 export interface User {
-  _id: ObjectId
+  _id: string // will use string instead of ObjectId other auth service return string
+  token: string // session token
+  name: string
+  avatar: string // avatar url
+  contact: string // email address
+  walletId?: string // wallet id !null user can receive money
+  income: number
+  bookings: ObjectId[] // one to many relationship
+  listings: ObjectId[] // one to many
 }
 
 export interface Database {
