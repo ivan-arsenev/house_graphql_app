a4e4a1cf7517d657d120a9894907fc1dc9cc980a
Ivan Arsenev <ivan_arsenev@pm.me>
2020-09-03 20:33:24 +0300

    Added clear script to clear all data in database

---
+++ server/temp/clear.ts
@@ -0,0 +1,25 @@
+require("dotenv").config()
+
+import { connectDatabase } from "../src/database"
+
+const clear = async () => {
+  try {
+    console.log("[clear] : running...")
+
+    const db = await connectDatabase()
+
+    const bookings = await db.bookings.find({}).toArray()
+    const listings = await db.listings.find({}).toArray()
+    const users = await db.users.find({}).toArray()
+
+    bookings.length > 0 && (await db.bookings.drop())
+    listings.length > 0 && (await db.listings.drop())
+    users.length > 0 && (await db.users.drop())
+
+    console.log("[clear] : success")
+  } catch {
+    throw new Error("failed to seed database")
+  }
+}
+
+clear()
