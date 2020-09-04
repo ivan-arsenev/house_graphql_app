d70c38d534e170ec9fa7555174d9be904a9430af
Ivan Arsenev <ivan_arsenev@pm.me>
2020-09-04 09:06:39 +0300

    Added google autherication and logIn resolver

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -2,22 +2,22 @@ import { gql } from "apollo-server-express"
 
 export const typeDefs = gql`
   type Viewer {
-    id: ID 
-    token:String
+    id: ID
+    token: String
     avatar: String
     hasWallet: Boolean
     didRequest: Boolean!
   }
 
   input LogInInput {
-    code : String!
+    code: String!
   }
   type Query {
     authUrl: String!
   }
 
   type Mutation {
-    logIn: (input: LogInInput): Viewer!
+    logIn(input: LogInInput): Viewer!
     logOut: Viewer!
   }
 `
