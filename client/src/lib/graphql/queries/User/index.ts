316dcfc11342c6c334212267f385b2de024b7e84
Ivan Arsenev <ivan_arsenev@pm.me>
2020-09-23 20:01:21 +0300

    Small fix

--- client/src/lib/graphql/queries/User/index.ts
+++ client/src/lib/graphql/queries/User/index.ts
@@ -1,7 +1,7 @@
 import { gql } from "apollo-boost"
 
 export const USER = gql`
-  query User($id: ID!) {
+  query User($id: ID!, $bookingsPage: Int!, $listingsPage: Int!, $limit: Int!) {
     user(id: $id) {
       id
       name
