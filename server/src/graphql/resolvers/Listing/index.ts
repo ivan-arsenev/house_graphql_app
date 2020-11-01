9b248e3207350c62e8b244b69681e098d2e3baf6
Ivan Arsenev <ivan_arsenev@pm.me>
2020-11-01 13:14:21 +0300

    Fixed listing resolver

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -1,7 +1,7 @@
 import { Database, Listing, User } from "../../../lib/types"
+import { ListingArgs, ListingBookingsArgs, ListingBookingsData } from "./types"
 
 import { IResolvers } from "apollo-server-express"
-import { ListingArgs } from "./types"
 import { ObjectId } from "mongodb"
 import { Request } from "express"
 import { authorize } from "../../../lib/utils"
@@ -33,7 +33,6 @@ export const listingResolvers: IResolvers = {
     id: (listing: Listing): string => {
       return listing._id.toString()
     },
-
     host: async (
       listing: Listing,
       _args: {},
@@ -45,9 +44,37 @@ export const listingResolvers: IResolvers = {
       }
       return host
     },
-
     bookingsIndex: (listing: Listing): string => {
       return JSON.stringify(listing.bookingsIndex)
+    },
+    bookings: async (
+      listing: Listing,
+      { limit, page }: ListingBookingsArgs,
+      { db }: { db: Database }
+    ): Promise<ListingBookingsData | null> => {
+      // Offset-based pagination (numbered pages)
+      try {
+        if (!listing.authorized) return null
+
+        const data: ListingBookingsData = {
+          total: 0,
+          result: [],
+        }
+
+        let cursor = await db.bookings.find({
+          _id: { $in: listing.bookings },
+        })
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0)
+        cursor = cursor.limit(limit)
+
+        data.total = await cursor.count()
+        data.result = await cursor.toArray()
+
+        return data
+      } catch (error) {
+        throw new Error(`Failed to query listing bookings: ${error}`)
+      }
     },
   },
 }
