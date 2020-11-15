1993fc67a26f5c191566ebd744de5acc6f991632
Ivan Arsenev <iv.arsenev@gmail.com>
2020-11-15 23:47:20 +0300

    Update index.tsx

--- client/src/sections/Listing/components/ListingBookings/index.tsx
+++ client/src/sections/Listing/components/ListingBookings/index.tsx
@@ -24,54 +24,58 @@ export const ListingBookings = ({
 
     const listingBookingsList = listingBookings ? (
         <List
-           
+            grid={{
+                gutter: 8,
+                xs: 1,
+                sm: 2,
+                lg: 3
             }}
-dataSource = { result? result: undefined }
-locale = {{ emptyText: "No bookings have been made yet" }}
-pagination = {{
-    current: bookingsPage,
-        total,
-        defaultPageSize: limit,
-            hideOnSinglePage: true,
+            dataSource={result ? result : undefined}
+            locale={{ emptyText: "No bookings have been made yet" }}
+            pagination={{
+                current: bookingsPage,
+                total,
+                defaultPageSize: limit,
+                hideOnSinglePage: true,
                 showLessItems: true,
-                    onChange: (page: number) => setBookingsPage(page)
-}}
-renderItem = { listingBooking => {
-    const bookingHistory =
-        <div className="listing-bookings__history">
-            <div>
-                Check in: <Text strong>
-                    {listingBooking.checkIn}
-                </Text>
-            </div>
-            <div>
-                Check out: <Text strong>
-                    {listingBooking.checkOut}
-                </Text>
-            </div>
-        </div>
-    return <List.Item>
-        {bookingHistory}
-        <Link to={`/user/${listingBooking.tenant.id}`} >
-            <Avatar src={listingBooking.tenant.avatar} size={64} shape="square" />
-        </Link>
-    </List.Item>
-}}
-/>
+                onChange: (page: number) => setBookingsPage(page)
+            }}
+            renderItem={listingBooking => {
+                const bookingHistory =
+                    <div className="listing-bookings__history">
+                        <div>
+                            Check in: <Text strong>
+                                {listingBooking.checkIn}
+                            </Text>
+                        </div>
+                        <div>
+                            Check out: <Text strong>
+                                {listingBooking.checkOut}
+                            </Text>
+                        </div>
+                    </div>
+                return <List.Item>
+                    {bookingHistory}
+                    <Link to={`/user/${listingBooking.tenant.id}`} >
+                        <Avatar src={listingBooking.tenant.avatar} size={64} shape="square" />
+                    </Link>
+                </List.Item>
+            }}
+        />
     ) : null
 
-const listingBookingsElement = listingBookingsList ? (
-    <div className="listing-bookings">
-        <Divider />
-        <div className='listing-bookings__section'>
-            <Title level={4} >
-                Bookings
+    const listingBookingsElement = listingBookingsList ? (
+        <div className="listing-bookings">
+            <Divider />
+            <div className='listing-bookings__section'>
+                <Title level={4} >
+                    Bookings
             </Title>
-        </div>
+            </div>
 
-        {listingBookingsList}
-    </div >
-) : null
+            {listingBookingsList}
+        </div >
+    ) : null
 
-return listingBookingsElement
+    return listingBookingsElement
 }
