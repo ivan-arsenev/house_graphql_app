6614342ba7bd31cc4329365e3fdfd3fe64e94be7
Ivan Arsenev <iv.arsenev@gmail.com>
2021-02-12 20:17:11 +0300

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider, useMutation } from "@apollo/react-hooks";
-import { StripeProvider, Elements } from "react-stripe-elements";
-import { Affix, Spin, Layout } from "antd";
+import React, { useState, useEffect, useRef } from "react"
+import { render } from "react-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import ApolloClient from "apollo-boost"
+import { ApolloProvider, useMutation } from "@apollo/react-hooks"
+import { StripeProvider, Elements } from "react-stripe-elements"
+import { Affix, Spin, Layout } from "antd"
 import {
   AppHeader,
   Home,
@@ -14,58 +14,58 @@ import {
   Login,
   NotFound,
   Stripe,
-  User
-} from "./sections";
-import { AppHeaderSkeleton, ErrorBanner } from "./lib/components";
-import { LOG_IN } from "./lib/graphql/mutations";
+  User,
+} from "./sections"
+import { AppHeaderSkeleton, ErrorBanner } from "./lib/components"
+import { LOG_IN } from "./lib/graphql/mutations"
 import {
   LogIn as LogInData,
-  LogInVariables
-} from "./lib/graphql/mutations/LogIn/__generated__/LogIn";
-import { Viewer } from "./lib/types";
-import * as serviceWorker from "./serviceWorker";
-import "./styles/index.css";
+  LogInVariables,
+} from "./lib/graphql/mutations/LogIn/__generated__/LogIn"
+import { Viewer } from "./lib/types"
+import * as serviceWorker from "./serviceWorker"
+import "./styles/index.css"
 
 const client = new ApolloClient({
   uri: "/api",
-  request: async operation => {
-    const token = sessionStorage.getItem("token");
+  request: async (operation) => {
+    const token = sessionStorage.getItem("token")
     operation.setContext({
       headers: {
-        "X-CSRF-TOKEN": token || ""
-      }
-    });
-  }
-});
+        "X-CSRF-TOKEN": token || "",
+      },
+    })
+  },
+})
 
 const initialViewer: Viewer = {
   id: null,
   token: null,
   avatar: null,
   hasWallet: null,
-  didRequest: false
-};
+  didRequest: false,
+}
 
 const App = () => {
-  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  const [viewer, setViewer] = useState<Viewer>(initialViewer)
   const [logIn, { error }] = useMutation<LogInData, LogInVariables>(LOG_IN, {
-    onCompleted: data => {
+    onCompleted: (data) => {
       if (data && data.logIn) {
-        setViewer(data.logIn);
+        setViewer(data.logIn)
 
         if (data.logIn.token) {
-          sessionStorage.setItem("token", data.logIn.token);
+          sessionStorage.setItem("token", data.logIn.token)
         } else {
-          sessionStorage.removeItem("token");
+          sessionStorage.removeItem("token")
         }
       }
-    }
-  });
-  const logInRef = useRef(logIn);
+    },
+  })
+  const logInRef = useRef(logIn)
 
   useEffect(() => {
-    logInRef.current();
-  }, []);
+    logInRef.current()
+  }, [])
 
   if (!viewer.didRequest && !error) {
     return (
@@ -75,14 +75,12 @@ const App = () => {
           <Spin size="large" tip="Launching Tinyhouse" />
         </div>
       </Layout>
-    );
+    )
   }
 
   const logInErrorBannerElement = error ? (
     <ErrorBanner description="We weren't able to verify if you were logged in. Please try again later!" />
-  ) : null;
-
-
+  ) : null
 
   return (
     <StripeProvider apiKey={process.env.REACT_APP_S_PUBLISHABLE_KEY as string}>
@@ -123,17 +121,17 @@ const App = () => {
         </Layout>
       </Router>
     </StripeProvider>
-  );
-};
+  )
+}
 
 render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
   document.getElementById("root")
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
